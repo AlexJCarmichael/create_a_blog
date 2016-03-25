2b9deee09855b9b9f65b6094ed848f8f9f9a1718
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-25 13:20:17 -0500

    Able to CRUD the Post class in json

--- app/controllers/api_posts_controller.rb
+++ app/controllers/api_posts_controller.rb
@@ -7,19 +7,67 @@ class APIPostsController < ApplicationController
   def show
     @post = App.posts.find { |p| p.id == params[:id].to_i }
     if @post
-      render_template 'posts/show.html.erb'
+      render @post.to_json
     else
-      render ""
+      render_not_found
     end
-  end
-
-  def new
-    render_template "posts/new.html.erb"
   end
 
   def create
     new_post = Post.new(params[:title], params[:author], params[:body])
     App.posts << new_post
     redirect_to "/posts/#{new_post.id}"
+  end
+
+  def update
+    post = App.posts.find { |p| p.id == params[:id].to_i }
+    if params[:body] && params[:author] && params[:title]
+      post.body = params[:body]
+      post.author = params[:author]
+      post.title = params[:title]
+      render post.to_json
+    elsif params[:body] && params[:title]
+      post.body = params[:body]
+      post.title = params[:title]
+      render post.to_json
+    elsif params[:title] && params[:author]
+      post.body = params[:body]
+      post.author = params[:author]
+      render post.to_json
+    elsif params[:body] && params[:author]
+      post.body = params[:body]
+      post.author = params[:author]
+      render post.to_json
+    elsif params[:body]
+      post.body = params[:body]
+      render post.to_json
+    elsif params[:title]
+      post.title = params[:title]
+      render post.to_json
+    elsif params[:author]
+      post.author = params[:author]
+      render post.to_json
+    else
+      render_not_found
+    end
+  end
+
+  def destroy
+    post = App.posts.find { |p| p.id == params[:id].to_i }
+    if post
+      App.posts.delete(post)
+        render post.to_json
+    else
+      render_not_found
+    end
+  end
+
+  private
+  def render_not_found
+    return_message = {
+      message: "Task not found!",
+      status: '404'
+    }.to_json
+    render return_message, status: "404 NOT FOUND"
   end
 end
